55c5566b9559d0e24081a40e6d5ed8f4a8c184f7
Harrison Shoebridge <harrison@theshoebridges.com>
2020-11-11 17:54:58 +1100

    Add basic flashable thing

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # legOS -- an operating system for STM32 ARM devices
+
+1. make sure your path includes the latest ARM sdk
+2. start st-util before running gdb
---
+++ build.sh
@@ -0,0 +1,12 @@
+PREFIX=arm-none-eabi
+
+CFLAGS="-mcpu=cortex-m4 -mthumb -Wall -O2 -ggdb"
+
+CC=$PREFIX-gcc
+LD=$PREFIX-ld
+
+SRC="start.S"
+
+$CC $CFLAGS -c $SRC
+
+$LD -nostdlib -nostartfiles start.o -T link.ld -o kernel.elf
